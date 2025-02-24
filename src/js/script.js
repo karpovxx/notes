6e9fc9c7d2da45ca09d61a05a3784314ea0f079d
Ruslan Karpov <ruslankarpov64@gmail.com>
2025-02-24 20:21:50 +0400

    add completed notes

--- src/js/script.js
+++ src/js/script.js
@@ -3,34 +3,48 @@ const inputElement = document.getElementById('title'),
 	createBtn = document.getElementById('create'),
 	listElement = document.getElementById('list')
 
-const notes = ['bread', 'apple', 'chocolate']
-let getNoteTemplate = title => {
-	return `<li
-          class="list-group-item d-flex justify-content-between align-items-center"
-        >
-          <span>${title}</span>
-          <span>
-            <span class="btn btn-small btn-success">&check;</span>
-            <span class="btn btn-small btn-danger">&times;</span>
-          </span>
-        </li>`
+// Функция для генерации HTML-шаблона заметки
+let getNoteTemplate = note => {
+	return `<li class="list-group-item d-flex justify-content-between align-items-center">
+              <span class="${
+								note.completed ? 'text-decoration-line-through' : ''
+							}">${note.title}</span>
+              <span>
+                <span class="btn btn-small btn-success">&check;</span>
+                <span class="btn btn-small btn-danger">&times;</span>
+              </span>
+            </li>`
 }
+
+// Массив заметок
+const notes = [
+	{ title: 'bread', completed: false },
+	{ title: 'apple', completed: true },
+]
+
+// Функция для рендера списка
 let render = () => {
+	listElement.innerHTML = '' // Очищаем список перед ререндерингом
 	for (let i = 0; i < notes.length; i++) {
-		listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]))
+		listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i])) // Передаем весь объект
 	}
 }
+
+// Отображаем начальные заметки
 render()
+
+// Добавление новой заметки
 createBtn.onclick = () => {
-	//Условие, которое не дет добавлять пустой input
 	if (inputElement.value.length === 0) {
 		return
 	}
-	listElement.insertAdjacentHTML(
-		'beforeend',
-		getNoteTemplate(inputElement.value)
-	)
 
-	//Чистка окна ввода после добавления заметки
+	// Добавляем новую заметку в массив
+	notes.push({ title: inputElement.value, completed: false })
+
+	// Перерисовываем список
+	render()
+
+	// Очищаем поле ввода
 	inputElement.value = ''
 }
