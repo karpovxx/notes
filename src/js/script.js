c8c0bfb09a6b73610e6e091e50324fe900cd48c6
Ruslan Karpov <ruslankarpov64@gmail.com>
2025-02-25 20:49:10 +0400

    adding dynamics to the app

--- src/js/script.js
+++ src/js/script.js
@@ -4,14 +4,16 @@ const inputElement = document.getElementById('title'),
 	listElement = document.getElementById('list')
 
 // Функция для генерации HTML-шаблона заметки
-let getNoteTemplate = note => {
+let getNoteTemplate = (note, index) => {
 	return `<li class="list-group-item d-flex justify-content-between align-items-center">
               <span class="${
 								note.completed ? 'text-decoration-line-through' : ''
 							}">${note.title}</span>
               <span>
-                <span class="btn btn-small btn-success">&check;</span>
-                <span class="btn btn-small btn-danger">&times;</span>
+                <span class="btn btn-small btn-${
+									note.completed ? 'warning' : 'success'
+								}" data-index="${index}" data-type="toggle">&check;</span>
+                <span class="btn btn-small btn-danger" data-type="remove" data-index="${index}">&times;</span>
               </span>
             </li>`
 }
@@ -24,9 +26,12 @@ const notes = [
 
 // Функция для рендера списка
 let render = () => {
-	listElement.innerHTML = '' // Очищаем список перед ререндерингом
+	listElement.innerHTML = ''
+	if (notes.length === 0) {
+		listElement.innerHTML = 'Нет элементов'
+	} // Очищаем список перед ререндерингом
 	for (let i = 0; i < notes.length; i++) {
-		listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i])) // Передаем весь объект
+		listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i)) // Передаем объект и индекс
 	}
 }
 
@@ -38,13 +43,34 @@ createBtn.onclick = () => {
 	if (inputElement.value.length === 0) {
 		return
 	}
-
+	const newNote = {
+		title: inputElement.value,
+		completed: false,
+	}
 	// Добавляем новую заметку в массив
-	notes.push({ title: inputElement.value, completed: false })
-
-	// Перерисовываем список
+	notes.push(newNote)
 	render()
 
 	// Очищаем поле ввода
 	inputElement.value = ''
+}
+
+// Обработчик кликов на списке
+listElement.onclick = event => {
+	if (event.target.dataset.index) {
+		const index = +event.target.dataset.index // Преобразуем индекс в число
+		const type = event.target.dataset.type // Получаем тип действия
+
+		if (type === 'toggle') {
+			// Переключаем статус завершения заметки
+			notes[index].completed = !notes[index].completed
+			console.log('toggle', index)
+			render()
+		} else if (type === 'remove') {
+			console.log('remove', index)
+			// Удаляем заметку из массива
+			notes.splice(index, 1) //Что удалить и сколько
+			render()
+		}
+	}
 }
